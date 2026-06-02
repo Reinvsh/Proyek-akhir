f9497681b6735f64d0ccbe14333390c82d0bc9ea
Nabhan1876 <simbahnabhan@gmail.com>
2026-06-02 16:39:34 +0700

    Menambahkan struct dan deklarasi ukuran array penyimpanan data buku dan deklarasi nama file

--- projekakhir.cpp
+++ projekakhir.cpp
@@ -3,6 +3,19 @@
 #include <string>
 using namespace std;
 
+struct Buku {
+
+    string judulBuku;
+    string namaPenulis;
+    string namaPenerbit;
+    int tahunTerbit;
+
+};
+
+const int MAX_BUKU = 30;
+const string NAMA_FILE = "arsipPerpustakaan.txt";
+
 int main(){
+
 
 }
